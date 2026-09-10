5b462e9bf20dd84366c5e1ff37b4164938b82809
kbrousse <kevin.brousse1@gmail.com>
2026-09-10 16:07:26 +0200

    Base server with tests

---
+++ src/server/createEndpoints.ts
@@ -0,0 +1,48 @@
+import {FastifyInstance} from "fastify";
+import {Routes} from "@server/http/routes/routes.ts";
+import {JoinRoomDto} from "@shared/http/JoinRoom.dto.ts";
+
+/**
+ * Creates all server endpoints.
+ */
+export function createEndpoints(server: FastifyInstance) {
+    /**
+     * Base root endpoint.
+     */
+    server.get(Routes.ROOT, async (): Promise<{message: string}> => {
+        return {
+            message: `Hello from ${Routes.ROOT}`
+        }
+    });
+
+    /**
+     * Room connection endpoint.
+     *
+     * roomId is a number between 1 and 9999. 0 and leading zeros are excluded.
+     * playerName is a string that contains letters and numbers, without any restrictions as to capitalization.
+     */
+    server.get<{Params: JoinRoomDto}>(Routes.JOIN_ROOM, {
+        schema: {
+            params: {
+                type: 'object',
+                required: ['roomId', 'playerName'] satisfies (keyof JoinRoomDto)[],
+                properties: {
+                    roomId: {
+                        type: 'string',
+                        pattern: '^(?!0)\\d{1,4}$'
+                    },
+                    playerName: {
+                        type: 'string',
+                        pattern: '^[a-zA-Z0-9]+$'
+                    }
+                }
+            }
+        }
+    }, async (req): Promise<{message: string}> => {
+        const { roomId, playerName } = req.params;
+
+        return {
+            message: `room is ${roomId} and playerName is ${playerName}`
+        }
+    });
+}
